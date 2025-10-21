a460808235d54935242d392afc6d93a0fe496010
Iris Monstad <irismonstad@gmail.com>
2025-10-21 14:10:34 +0200

    Gjort ferdig funksjonalitet for stedsnavn m brukerinput side

--- javascript/stedsnavn/script.js
+++ javascript/stedsnavn/script.js
@@ -1,12 +1,24 @@
-let navn1 = document.getElementById("navn1").value;
-let navn2 = document.getElementById("navn2").value;
-let differanse = Math.abs(navn1.length - navn2.length)
+function getNavn1(){
+    return document.getElementById("navn1").value;
+}
+
+function getNavn2(){
+    return document.getElementById("navn2").value;
+}
 
 const beregnknapp = document.getElementById("beregnknapp");
 beregnknapp.addEventListener("click", beregn);
 
-function beregn(navn1, navn2, differanse) {
-    let output = `Ett sted heter ${navn1} og et annet heter ${navn2}. Den første plassen består av ${navn1.length} tegn og den andre består av ${navn2.length} tegn. Differansen mellom disse to er ${differanse}`
-    document.getElementById("utskrift").innerText = output;
-}
+document.getElementById("utskrift").innerText = "Du har ikke sjekket en differanse enda.";
 
+function beregn() {
+    let navn1 = getNavn1()
+    let navn2 = getNavn2()
+    let differanse = Math.abs(navn1.length - navn2.length)
+    output = `Ett sted heter ${navn1} og et annet heter ${navn2}. Den første plassen består av ${navn1.length} tegn og den andre består av ${navn2.length} tegn. Differansen mellom disse to er ${differanse}`
+    if (navn1.length > 0 && navn2.length > 0){
+        document.getElementById("utskrift").innerText = output;
+    } else{
+        document.getElementById("utskrift").innerText = "Du har ingenting i ett eller flere av feltene.";
+    }
+}
