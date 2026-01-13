eabb9b31da7d40b922d5495a7f33b9e45e1cb83c
Iris Monstad <irismonstad@gmail.com>
2026-01-13 15:19:43 +0100

    Program som lar bruker vise abilities til en selvvalgt pokemon

---
+++ javascript/pokemon/pokemon.js
@@ -0,0 +1,29 @@
+function sendToFunction(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const abilities = document.getElementById("ability").value;
+    getPokemon(name, abilities)
+}
+
+async function getPokemon(name, abilities) {
+    document.getElementById("output").innerHTML = "";
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+    const data = await response.json();
+
+    let outputname = document.createElement('h1');
+    outputname.innerText = data.forms[0].name;
+    document.getElementById("output").appendChild(outputname);
+
+    if (abilities === "yes") {
+        let abilityoutput = document.createElement("ul");
+        for (let i = 0; i < data.abilities.length; i++) {
+            let ability = data.abilities[i].ability.name;
+            abilityoutput.innerHTML += `<li>${ability}</li>`;
+        }
+        document.getElementById("output").appendChild(abilityoutput);
+
+    }
+
+}
+
+document.getElementById("form").addEventListener('submit', sendToFunction);
